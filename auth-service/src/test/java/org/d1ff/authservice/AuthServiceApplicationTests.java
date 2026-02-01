a559308b081dd8eabce4d9769099133f8f04933f
D1ff1cult42 <orbitovkek@gmail.com>
2026-02-01 13:52:37 +0300

    initial commit

---
+++ auth-service/src/test/java/org/d1ff/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.d1ff.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
