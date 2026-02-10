1d0655747b19420762ac1233b78c6477dfc0d419
D1ff1cult42 <orbitovkek@gmail.com>
2026-02-11 00:17:11 +0300

    :sparkles: add entities
    -message
    -messageType
    -deliveryStatus
    -messageStatus

---
+++ message-service/src/main/java/org/d1ff/messageservice/entity/MessageStatus.java
@@ -0,0 +1,36 @@
+package org.d1ff.messageservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "message_statuses",
+       uniqueConstraints = @UniqueConstraint(columnNames = {"message_id", "user_id"}))
+public class MessageStatus {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "message_id", nullable = false)
+    private Message message;
+
+    @Column(name = "user_id")
+    private UUID userId;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private DeliveryStatus status = DeliveryStatus.SENT;
+
+    @Column(name = "read_at")
+    private LocalDateTime readAt;
+}
---
+++ message-service/src/main/java/org/d1ff/messageservice/entity/MessageType.java
@@ -0,0 +1,8 @@
+package org.d1ff.messageservice.entity;
+public enum MessageType {
+    TEXT,
+    IMAGE,
+    VIDEO,
+    AUDIO,
+    DOCUMENT
+}
