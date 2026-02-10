119cf063fac44de6f4a82c334b8f4fcc5ffb593e
D1ff1cult42 <orbitovkek@gmail.com>
2026-02-11 00:19:39 +0300

    :sparkles: add repositories for message and messageStatus

---
+++ message-service/src/main/java/org/d1ff/messageservice/repository/MessageRepository.java
@@ -0,0 +1,22 @@
+package org.d1ff.messageservice.repository;
+
+import org.d1ff.messageservice.entity.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
+
+    Page<Message> findAllByFromUser(UUID userId, Pageable pageable);
+
+    @Query("SELECT m FROM Message m WHERE m.id= :id AND m.deleted = false")
+    Optional<Message> findByIdAndNotDeleted(@Param("id") Long id);
+    Page<Message> findAllByChatId(UUID chatId, Pageable pageable);
+}
