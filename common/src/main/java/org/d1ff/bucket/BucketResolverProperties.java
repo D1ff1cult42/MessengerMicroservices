97f02498c68a3c8492bb36b242942aab669d76c3
D1ff1cult42 <orbitovkek@gmail.com>
2026-02-13 21:16:10 +0300

    :boom: add abstract bucket resolver for all services

---
+++ common/src/main/java/org/d1ff/bucket/BucketResolverProperties.java
@@ -0,0 +1,13 @@
+package org.d1ff.bucket;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+@ConfigurationProperties(prefix = "bucket-resolver")
+public class BucketResolverProperties {
+    private Map<String, BucketConfig> buckets = new HashMap<>();
+}
