f3c3aefa7da9edb662c4eac7dfe9074c335ad0ba
D1ff1cult42 <orbitovkek@gmail.com>
2026-02-18 23:01:03 +0300

    :bug: fix chat response mapper(haven't pageable, compilation failed)

--- chat-service/src/main/java/com/d1ff/chatservice/mapper/response/ChatResponseMapper.java
+++ chat-service/src/main/java/com/d1ff/chatservice/mapper/response/ChatResponseMapper.java
@@ -4,14 +4,16 @@ import com.d1ff.chatservice.dto.response.ChatResponse;
 import com.d1ff.chatservice.entity.Chat;
 import com.d1ff.chatservice.repository.ChatParticipantRepository;
 import org.mapstruct.*;
+import org.springframework.data.domain.Pageable;
 
 @Mapper(componentModel = "spring",
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface ChatResponseMapper {
     @Mappings({
-            @Mapping(target = "participants", expression = "java(chatParticipantRepository.findByChatId(chat.getId()).map(participantMapper::toChatParticipantResponse))")
+            @Mapping(target = "participants", expression = "java(chatParticipantRepository.findByChatId(chat.getId(), pageable).map(participantMapper::toChatParticipantResponse))")
     })
     ChatResponse toChatResponse(Chat chat,
+                                @Context Pageable pageable,
                                 @Context ChatParticipantResponseMapper participantMapper,
                                 @Context ChatParticipantRepository chatParticipantRepository);
 }
