5e6ff55116b120e40381d20955113cec20cb2217
D1ff1cult42 <orbitovkek@gmail.com>
2026-02-19 23:01:13 +0300

    :bug: fix chat response mapper(doesn't use presigned url in response)

--- chat-service/src/main/java/com/d1ff/chatservice/mapper/response/ChatResponseMapper.java
+++ chat-service/src/main/java/com/d1ff/chatservice/mapper/response/ChatResponseMapper.java
@@ -2,6 +2,7 @@ package com.d1ff.chatservice.mapper.response;
 
 import com.d1ff.chatservice.dto.response.ChatResponse;
 import com.d1ff.chatservice.entity.Chat;
+import com.d1ff.chatservice.grpc.FileGrpcClient;
 import com.d1ff.chatservice.repository.ChatParticipantRepository;
 import org.d1ff.page.PageResponse;
 import org.mapstruct.*;
@@ -11,10 +12,12 @@ import org.springframework.data.domain.Pageable;
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface ChatResponseMapper {
     @Mappings({
-            @Mapping(target = "participants", expression = "java(PageResponse.fromPage(chatParticipantRepository.findByChatId(chat.getId(), pageable).map(participantMapper::toChatParticipantResponse)))")
+            @Mapping(target = "participants", expression = "java(PageResponse.fromPage(chatParticipantRepository.findByChatId(chat.getId(), pageable).map(participantMapper::toChatParticipantResponse)))"),
+            @Mapping(target = "presignedIconUrl", expression = "java(chat.getIconBucketName() == null || chat.getIconObjectName() == null ? null : fileService.getPresignedUrl(chat.getIconBucketName(), chat.getIconObjectName()))")
     })
     ChatResponse toChatResponse(Chat chat,
                                 @Context Pageable pageable,
                                 @Context ChatParticipantResponseMapper participantMapper,
-                                @Context ChatParticipantRepository chatParticipantRepository);
+                                @Context ChatParticipantRepository chatParticipantRepository,
+                                @Context FileGrpcClient fileService);
 }
