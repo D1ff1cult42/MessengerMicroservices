18a7272a2edf574811188f1a299e3a6dddeccf39
D1ff1cult42 <orbitovkek@gmail.com>
2026-02-26 00:55:59 +0300

    :bug: :card_file_box: fix migration with change generation algorithm from identity to sequence

--- message-service/src/main/resources/db/migration/V2__message_status_sequence.sql
+++ message-service/src/main/resources/db/migration/V2__message_status_sequence.sql
@@ -1,3 +1,15 @@
-CREATE SEQUENCE message_statuses_id_seq START WITH 1 INCREMENT BY 50;
+ALTER TABLE message_statuses ALTER COLUMN id DROP IDENTITY IF EXISTS;
+
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM pg_class c
+        JOIN pg_namespace n ON n.oid = c.relnamespace
+        WHERE c.relkind = 'S' AND c.relname = 'message_statuses_id_seq'
+    ) THEN
+        CREATE SEQUENCE message_statuses_id_seq START WITH 1 INCREMENT BY 50;
+    END IF;
+END
+$$;
 
 ALTER TABLE message_statuses ALTER COLUMN id SET DEFAULT nextval('message_statuses_id_seq');
