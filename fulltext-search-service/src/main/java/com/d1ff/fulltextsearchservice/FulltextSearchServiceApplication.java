505956f991ff63babcac9a33e95f03c94b8212b0
D1ff1cult42 <orbitovkek@gmail.com>
2026-03-01 01:09:35 +0300

    :tada: initial commit

---
+++ fulltext-search-service/src/main/java/com/d1ff/fulltextsearchservice/FulltextSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.d1ff.fulltextsearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulltextSearchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FulltextSearchServiceApplication.class, args);
+    }
+
+}
---
+++ fulltext-search-service/src/test/java/com/d1ff/fulltextsearchservice/FulltextSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d1ff.fulltextsearchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FulltextSearchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
