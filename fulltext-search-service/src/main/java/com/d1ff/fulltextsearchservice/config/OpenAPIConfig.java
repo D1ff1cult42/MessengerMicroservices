12b4309c1d258a0906f596e487b625f8452c0785
D1ff1cult42 <orbitovkek@gmail.com>
2026-03-02 22:01:07 +0300

    :memo: add open api configuration

---
+++ fulltext-search-service/src/main/java/com/d1ff/fulltextsearchservice/config/OpenAPIConfig.java
@@ -0,0 +1,38 @@
+package com.d1ff.fulltextsearchservice.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+    private static final String SECURITY_SCHEME_NAME = "Bearer Authentication";
+
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Fulltext-search-service API")
+                        .version("1.0")
+                        .description("Сервис полнотекстового поиска сообщений " +
+                                "Все endpoints в данном сервисе являются защищеннымми, используйте кнопку 'Authorize' " +
+                                "и вставьте access token. Также, есть endpoint для администратора, требующий роль ADMIN, " +
+                                "для доступа к нему необходимо использовать JWT токен, полученный при регистрации/логине пользователя с ролью ADMIN.")
+                        .contact(new Contact()
+                                .name("D1ff1cult42")
+                                .email("orbitovkek@gmail.com")))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+                                .name(SECURITY_SCHEME_NAME)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("Введите JWT токен, полученный при login/register")));
+    }
+}
