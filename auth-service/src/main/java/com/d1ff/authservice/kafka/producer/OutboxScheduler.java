376e688ced9d8b71ba3f36807588315ae86fc03b
D1ff1cult42 <orbitovkek@gmail.com>
2026-03-23 21:30:06 +0300

    :sparkles: :package: add kafka producer analytic-sent logic
    analytic sent producer(to save event into outbox table)
    :recycle: rework outbox scheduler to analytic-sent topic

--- auth-service/src/main/java/com/d1ff/authservice/kafka/producer/OutboxScheduler.java
+++ auth-service/src/main/java/com/d1ff/authservice/kafka/producer/OutboxScheduler.java
@@ -2,9 +2,11 @@ package com.d1ff.authservice.kafka.producer;
 
 import com.d1ff.authservice.entity.OutboxEvent;
 import com.d1ff.authservice.repository.OutboxEventRepository;
+import com.d1ff.common.avro.AnalyticSentEvent;
 import com.d1ff.common.avro.EmailConfirmationEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.specific.SpecificRecord;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -26,10 +28,9 @@ public class OutboxScheduler {
         List<OutboxEvent> outboxEvents = outboxEventRepository.findTop100BySentFalseOrderByCreatedAtAsc();
         for (OutboxEvent outboxEvent : outboxEvents) {
             try{
-                EmailConfirmationEvent emailConfirmationEvent = EmailConfirmationEvent
-                        .fromByteBuffer(ByteBuffer.wrap(outboxEvent.getPayload()));
+                SpecificRecord event = deserialize(outboxEvent);
 
-                kafkaTemplate.send(outboxEvent.getTopic(), outboxEvent.getAggregateId(), emailConfirmationEvent)
+                kafkaTemplate.send(outboxEvent.getTopic(), outboxEvent.getAggregateId(), event)
                         .get();
                 outboxEvent.setSent(true);
 
@@ -38,5 +39,14 @@ public class OutboxScheduler {
                 break;
             }
         }
+    }
+
+    private SpecificRecord deserialize(OutboxEvent outboxEvent) throws Exception {
+        ByteBuffer payload = ByteBuffer.wrap(outboxEvent.getPayload());
+        return switch(outboxEvent.getTopic()){
+            case "email-confirmation" -> EmailConfirmationEvent.fromByteBuffer(payload);
+            case "analytic-sent" -> AnalyticSentEvent.fromByteBuffer(payload);
+            default -> throw new IllegalStateException("Unknown topic: " + outboxEvent.getTopic());
+        };
     }
 }
