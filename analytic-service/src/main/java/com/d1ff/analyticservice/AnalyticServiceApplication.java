701715f086dff5163a701053a520627ec8d64e8e
D1ff1cult42 <orbitovkek@gmail.com>
2026-03-23 22:26:25 +0300

    :beers: add scheduling and configuration properties inside the service

--- analytic-service/src/main/java/com/d1ff/analyticservice/AnalyticServiceApplication.java
+++ analytic-service/src/main/java/com/d1ff/analyticservice/AnalyticServiceApplication.java
@@ -1,13 +1,17 @@
 package com.d1ff.analyticservice;
 
+import com.d1ff.analyticservice.config.AnalyticConfigurationProperties;
+import com.d1ff.bucket.BucketResolverProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableConfigurationProperties(AnalyticConfigurationProperties.class)
 public class AnalyticServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AnalyticServiceApplication.class, args);
     }
-
 }
